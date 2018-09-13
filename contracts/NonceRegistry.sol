ed35bf0395a532c9b98f5e7ba4b526e32d8811f8
Li Xuanji <xuanji@gmail.com>
2018-09-13 16:43:42 -0400

    fix bad rebase

--- contracts/NonceRegistry.sol
+++ contracts/NonceRegistry.sol
@@ -51,61 +51,6 @@ contract NonceRegistry {
     return uint256(bytes32(input) & bytes32(mask));
   }
 
-
-  /// @return Whether the Nth highest-order bit in input is set
-  function bitSet(uint256 self, uint8 index) public pure returns (bool) {
-    return self >> index & 1 == 1;
-  }
-
-  /// @return
-  ///   - nonce[0:120] == expectedR
-  ///   - nonce[128+i] == 1
-  function isFinalized120i(bytes32 key, uint256 expectedR, uint8 i) public view returns (bool) {
-    State storage state = table[key];
-
-    require(
-      table[key].finalizesAt <= block.number,
-      "Nonce is not yet finalized"
-    );
-    require(
-      getFirstNBits(state.nonce, 120) == getFirstNBits(expectedR, 120),
-      "nonce[0:120] is not equal to expectedR[0:120]"
-    );
-    require(
-      bitSet(state.nonce, 128 + i),
-      "nonce[128+i] is not equal to 1"
-    );
-    return true;
-  }
-
-  /// @return
-  ///   - nonce[0:128] == expectedR
-  function isFinalized128(bytes32 key, uint256 expectedR) public view returns (bool) {
-    State storage state = table[key];
-
-    require(
-      table[key].finalizesAt <= block.number,
-      "Nonce is not yet finalized"
-    );
-    require(
-      getFirstNBits(state.nonce, 128) == getFirstNBits(expectedR, 128),
-      "Nonce is not equal to expectedNonce"
-    );
-    return true;
-  }
-
-  /// @notice Return the N highest-order bits from the input.
-  /// @param input A uint256 treated as a bitfield from which to get the high-order bits
-  /// @param N the number of bits to get from input
-  /// @return A uint256 where the N highest-order bits are the same as in input, and
-  /// the other bits are 0
-  function getFirstNBits(uint256 input, uint8 N) public pure returns (uint256) {
-    uint256 nOnes = uint256(2) ** N - 1;
-    uint256 mask = nOnes << (uint8(256) - N); // uint8(256) == uint8(0)
-    return uint256(bytes32(input) & bytes32(mask));
-  }
-
-
   /// @return Whether the Nth highest-order bit in input is set
   function bitSet(uint256 self, uint8 index) public pure returns (bool) {
     return self >> index & 1 == 1;

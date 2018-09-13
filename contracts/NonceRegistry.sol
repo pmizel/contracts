7da0d5ca6f2ebac5f05937d2491adc366e977879
Li Xuanji <xuanji@gmail.com>
2018-09-13 14:59:12 -0400

    custom timeouts

--- contracts/NonceRegistry.sol
+++ contracts/NonceRegistry.sol
@@ -7,15 +7,12 @@ pragma experimental "ABIEncoderV2";
 /// @notice Supports a global mapping of sender and salt based keys to sequential nonces and the ability to consider a key "finalized" or not at a particular nonce
 contract NonceRegistry {
 
-  event NonceSet (bytes32 key, uint256 nonce);
-  event NonceFinalized (bytes32 key);
+  event NonceSet (bytes32 key, uint256 nonceValue);
 
   struct State {
-    uint256 nonce;
+    uint256 nonceValue;
     uint256 finalizesAt;
   }
-
-  uint256 private constant TIMEOUT = 10;
 
   mapping(bytes32 => State) public table;
 
@@ -28,13 +25,12 @@ contract NonceRegistry {
     view
     returns (bool)
   {
-    State storage state = table[key];
     require(
       table[key].finalizesAt <= block.number,
       "Nonce is not yet finalized"
     );
     require(
-      state.nonce == expectedNonce,
+      table[key].nonceValue == expectedNonce,
       "Nonce is not equal to expectedNonce"
     );
     return true;
@@ -43,31 +39,23 @@ contract NonceRegistry {
   /// @notice Set a nonce in the mapping and triggers the timeout period to begin
   /// @param salt A salt used to generate the nonce key
   /// @param nonce A nonce at which to set the computed key's value in the mapping
-  function setNonce(bytes32 salt, uint256 nonce) external {
-    bytes32 key = computeKey(salt);
-    require(table[key].nonce < nonce);
-    table[key].nonce = nonce;
-    table[key].finalizesAt = block.number + TIMEOUT;
+  function setNonce(uint256 timeout, bytes32 salt, uint256 nonceValue) external {
+    bytes32 key = computeKey(msg.sender, timeout, salt);
+    require(table[key].nonceValue < nonceValue);
+    table[key].nonceValue = nonceValue;
+    table[key].finalizesAt = block.number + timeout;
     emit NonceSet(key, nonce);
-  }
-
-  /// @notice Finalize a keys value in the mapping at a particular nonce without a timeout
-  /// @param salt A salt used to generate the nonce key
-  function finalizeNonce(bytes32 salt) external {
-    bytes32 key = computeKey(salt);
-    table[key].finalizesAt = block.number;
-    emit NonceFinalized(key);
   }
 
   /// @notice Computes a unique key for the particular salt and msg.sender
   /// @param salt A salt used to generate the nonce key
   /// @return A unique nonce key derived from the salt and msg.sender
-  function computeKey(bytes32 salt)
+  function computeKey(address sender, uint256 timeout, bytes32 salt)
     view
     internal
     returns (bytes32)
   {
-    return keccak256(abi.encodePacked(msg.sender, salt));
+    return keccak256(abi.encodePacked(sender, timeout, salt));
   }
 
 }
